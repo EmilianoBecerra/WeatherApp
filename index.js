faee3f2ec64b1ccc68400c0bfdb7f84d80067159
Emiliano Becerra <emibecerra92@gmail.com>
2024-09-08 20:55:23 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const apiKey = 'b451cc9f3bb87ac911e922ba2a832e59';
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
+
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
+
+async function checkWeather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector('.error').style.display = 'block';
+    document.querySelector('.weather').style.display = 'none';
+  } else {
+    let data = await response.json();
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML =
+      Math.round(data.main.temp) + '°C';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + ' %';
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
+
+    switch (data.weather[0].main) {
+      case 'Clouds':
+        weatherIcon.src = './imgs/clouds.png';
+        break;
+      case 'Clear':
+        weatherIcon.src = './imgs/clear.png';
+        break;
+      case 'Rain':
+        weatherIcon.src = './imgs/rain.png';
+        break;
+      case 'Drizzle':
+        weatherIcon.src = './imgs/drizzle.png';
+        break;
+      case 'Snow':
+        weatherIcon.src = './imgs/snow.png';
+        break;
+      default:
+        break;
+    }
+
+    document.querySelector('.weather').style.display = 'block';
+    document.querySelector('.error').style.display = 'none'
+  }
+}
+
+searchBtn.addEventListener('click', (ev) => {
+  checkWeather(searchBox.value);
+});
